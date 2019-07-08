cbba6c60b920eca92108f205cc61146230e62c40
Aileen Murphy <ai.murf2@gmail.com>
2019-07-07 17:16:09 -0700

    setup linkedlists

---
+++ code-challenges/401/Data-Structures/linkedList/linked-list.js
@@ -0,0 +1,8 @@
+// Features
+// Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+// Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
+// Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+// Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+// Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List.
+// At no time should an exception or stack trace be shown to the end user. Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
+// Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
