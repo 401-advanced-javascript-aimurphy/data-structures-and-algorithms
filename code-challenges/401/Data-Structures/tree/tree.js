7565e69eeefa2a5f4f8b3555d6f79435f9a958fb
Aileen Murphy <ai.murf2@gmail.com>
2019-08-16 23:20:36 -0700

    finally got insert and able to run code but still need to write tests

--- code-challenges/401/Data-Structures/tree/tree.js
+++ code-challenges/401/Data-Structures/tree/tree.js
@@ -1,140 +1,167 @@
 'use strict';
 
-class Node{
-  constructor(value){
+class Node {
+  constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
-class Queue{
-  constructor(){
+class Queue {
+  constructor() {
     // this.front = null;
     this.back = null;
     this.queue = [];
   }
-  enqueue(value){
+  enqueue(value) {
     this.queue.push(value);
     this.back = value;
     console.log(this.back);
   }
-  dequeue(){
+  dequeue() {
     let value = this.queue.shift();
     this.front = this.queue[0];
     return value;
   }
-  peek(){
+  peek() {
     return this.queue[0];
   }
 }
 
-class BinaryTree{
-  constructor(root=null){
-    this.root=root;
-    this.max=max;
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+    this.max = max;
   }
 
-  insert(value){
-    
+  insert(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+
+      let inTree = new Queue;
+
+      inTree.enqueue(tree.root);
+      let current;
+
+      while (inTree.peek()) {
+        current = inTree.dequeue();
+
+        if (current.left === null) {
+          current.left = new Node(value);
+          break;
+        } else {
+          inTree.enqueue(current.left);
+        };
+        if (current.right === null) {
+          current.right = new Node(value);
+          break;
+        } else {
+          inTree.enqueue(current.right);
+        };
+      }
+    }
+
   }
-  
-  preorder(){
-    let results = [];
-    
-    let _walk =(node)=> {
+
+}
+preorder(){
+  let results = [];
+
+  let _walk = (node) => {
     results.push(node.value);
 
-    if(node.left)_walk(node.left);
-    if(node.right) _walk(node.right);
-    };
-
-    _walk(this.root);
-    return results;
-  }
-
-  inOrder(){
-    let results = [];
-
-    let _walk=(node)=>{
-      if(node.left)_walk(node.left);
-      results.push(node.value);
-
-      if(node.right)_walk(node.right);
-      results.push(node.value);
-    }
-
-    _walk(this.root);
-    return results;
-  }
-
-  postOrder(){
-    let results = [];
-    
-    let _walk =(node)=> {
-      if(node.left)_walk(node.left);
-      if(node.right)_walk(node.right);
-      results.push(node.value);
-    };
-  
-    _walk(this.root);
-  
-    return results;
+    if (node.left) _walk(node.left);
+    if (node.right) _walk(node.right);
   };
 
-  levelOrder(){
+  _walk(this.root);
+  return results;
+}
 
-    let results = [];
-    let nodeQueue=[];
+inOrder(){
+  let results = [];
 
-    nodeQueue.push(this.root);
+  let _walk = (node) => {
+    if (node.left) _walk(node.left);
+    results.push(node.value);
 
-    while(nodeQueue.lenth){
-      let current=nodeQueue.shift();
-      results.push(current.value);
-      if(current.left)nodeQueue.push(current.left);
-      if(current.right)nodeQueue.push(current.right);
-    }
-    return results;
+    if (node.right) _walk(node.right);
+    results.push(node.value);
   }
 
+  _walk(this.root);
+  return results;
+}
 
-  breadthFirst(){
-    // code things
+postOrder(){
+  let results = [];
 
-    if(!this.root)return null;
+  let _walk = (node) => {
+    if (node.left) _walk(node.left);
+    if (node.right) _walk(node.right);
+    results.push(node.value);
+  };
 
-    let inTree = new Queue;
-    let outTree = new Queue;
+  _walk(this.root);
 
-    inTree.enqueue(tree.root);
-    let current;
+  return results;
+};
 
-    while(inTree.peek()){
-      current = inTree.dequeue();
+levelOrder(){
 
-      if (current.left)inTree.enqueue(current.left);
-      if(current.right)inTree.enqueue(current.right);
+  let results = [];
+  let nodeQueue = [];
 
-      console.log(current.value);
-      outTree.enqueue(current);
-    }
+  nodeQueue.push(this.root);
+
+  while (nodeQueue.lenth) {
+    let current = nodeQueue.shift();
+    results.push(current.value);
+    if (current.left) nodeQueue.push(current.left);
+    if (current.right) nodeQueue.push(current.right);
   }
+  return results;
+}
 
-  findMaximumValue(){
 
-    if(!this.root)return null;
+breadthFirst(){
+  // code things
 
-    this.max = 0;
+  if (!this.root) return null;
 
-    let _walk =(node)=> {
-      if (node.value>this.max)this.max=node.value;
-      if(node.left)_walk(node.left);
-      if(node.right) _walk(node.right);
-    };
+  let inTree = new Queue;
+  let outTree = new Queue;
 
-    _walk(this.root);
-    
-    return this.max;
+  inTree.enqueue(tree.root);
+  let current;
+
+  while (inTree.peek()) {
+    current = inTree.dequeue();
+
+    if (current.left) inTree.enqueue(current.left);
+    if (current.right) inTree.enqueue(current.right);
+
+    console.log(current.value);
+    outTree.enqueue(current);
   }
+}
+
+findMaximumValue(){
+
+  if (!this.root) return null;
+
+  let max = 0;
+
+  let _walk = (node) => {
+    if (node.value > max) max = node.value;
+    if (node.left) _walk(node.left);
+    if (node.right) _walk(node.right);
+  };
+
+  _walk(this.root);
+
+  return max;
+}
 }
