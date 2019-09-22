9e09565983948d2c5260cec6d0228e55ca513426
Aileen Murphy <ai.murf2@gmail.com>
2019-09-22 13:46:39 -0700

    working on quick sort

--- code-challenges/401/Data-Structures/allSortsOfSorts/quickSort/challenge-28.js
+++ code-challenges/401/Data-Structures/allSortsOfSorts/quickSort/challenge-28.js
@@ -1,32 +1,41 @@
 'use strict';
-// ALGORITHM QuickSort(arr, left, right)
-//     if left < right
-//         // Partition the array by setting the position of the pivot value 
-//         DEFINE position <-- Partition(arr, left, right)
-//         // Sort the left
-//         QuickSort(arr, left, position - 1)
-//         // Sort the right
-//         QuickSort(arr, position + 1, right)
 
-// ALGORITHM Partition(arr, left, right)
-//     // set a pivot value as a point of reference
-//     DEFINE pivot <-- arr[right]
-//     // create a variable to track the largest index of numbers lower than the defined pivot
-//     DEFINE low <-- left - 1
-//     for i <- left to right do
-//         if arr[i] <= pivot
-//             low++
-//             Swap(arr, i, low)
+// const arr = [5, 4, 9, 1, 2];
 
-//      // place the value of the pivot location in the middle.
-//      // all numbers smaller than the pivot are on the left, larger on the right. 
-//      Swap(arr, right, low + 1)
-//     // return the pivot index point
-//      return low + 1
+function swap(arr, i, j){
+   [arr[i],arr[j]] = [arr[j],arr[i]];
+}
 
-// ALGORITHM Swap(arr, i, low)
-//     DEFINE temp;
-//     temp <-- arr[i]
-//     arr[i] <-- arr[low]
-//     arr[low] <-- temp
+// partition calls on swap
+function partition(arr, pivot, left, right){
+   var pivotValue = arr[pivot],
+       partitionIndex = left;
 
+   for(var i = left; i < right; i++){
+    if(arr[i] < pivotValue){
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+ 
+// qsort calls on partition which calls on swap
+function qsort(arr,left,right){
+   var len = arr.length, 
+   pivot,
+   partitionIndex;
+
+  if(left < right){
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+    
+   //sort left and right
+   qsort(arr, left, partitionIndex - 1);
+   qsort(arr, partitionIndex + 1, right);
+  }
+  return arr;
+}
+
+// qsort(arr,0,arr.length-1);
